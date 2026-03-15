e9f04e758edd5452678906bb8fc5c68323556043
gobinaath2003 <gobinaath1234@gmail.com>
2026-03-15 21:31:27 +0530

    port added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
 from database import db
 from route.routes import auth_bp
+import os
 
 app = Flask(__name__)
 
@@ -20,4 +21,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
